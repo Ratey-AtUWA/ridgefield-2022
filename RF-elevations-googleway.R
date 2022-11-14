cb1320ad373d81e383fce2ae702f700b78b63d79
Andrew Rate <andrew.rate@uwa.edu.au>
2022-11-14 17:01:51 +0800

    adjust size of elevation grid, correct plot

--- RF-elevations-googleway.R
+++ RF-elevations-googleway.R
@@ -1,7 +1,7 @@
 library(googleway)
 
-lats0 <- seq(-32.5161, -32.4613, len=50) # gives latitudes of w-e transects
-lons0 <- seq(116.945, 117.0085, len=50) # gives longitudes of n-s transects
+lats0 <- seq(-32.515, -32.465, len=50) # gives latitudes of w-e transects
+lons0 <- seq(116.94, 117.01, len=50) # gives longitudes of n-s transects
 # 
 e1 <- rep(NA,2500)
 lat1 <- rep(NA,2500)
@@ -11,7 +11,7 @@ for(i in 1:NROW(lons0)) {
   for(j in 1:NROW(lats0)) {
     pt0 <- as.data.frame(cbind(lats0[j],lons0[i]))
     colnames(pt0)[1:2] <- c("lat","lon")
-    #       use new api key for andrewwrate@gmail.com 20190123
+    #       use own api key
     elev.TEMP <- as.data.frame(google_elevation(df_locations = pt0,
                                                 location_type="individual",              
                                                 key = GoogleElevKey,
@@ -30,7 +30,7 @@ print(summary(elev.TEMP[,3]), digits=9)
 #
 # add utm coordinates to the data frame and save to a csv file
 library(sf)
-TEMP.LL<-extent <- st_as_sf(x = elev.TEMP, 
+TEMP.LL <- st_as_sf(x = elev.TEMP, 
                             coords = c("Longitude", "Latitude"), crs = LongLat)
 summary(st_coordinates(TEMP.LL))
 TEMP.utm <- st_transform(TEMP.LL, crs = UTM50S)
@@ -40,8 +40,17 @@ elev.TEMP <- cbind(round(st_coordinates(TEMP.utm),1),elev.TEMP)
 colnames(elev.TEMP)[1:2] <- c("Easting","Northing")
 write.csv(elev.TEMP, file="elev_RF.csv", row.names=FALSE)
 
-with(elev.TEMP, plot(Easting, Northing, asp=1))
-image(elev.TEMP[,1],elev.TEMP[,2],elev.TEMP[,3])
-
+par(oma=c(3,3,1,1), mar=c(4,4,1.5,1.5), mgp=c(1.4,0.3,0), 
+    lend=2, ljoin=1, tcl=0.3)
+plot(rftiles);axis(1);axis(2)
+with(rf_boundary, lines(Easting, Northing, col="#00000040", lwd=3))
+contour(x = seq(min(elev.TEMP[,1]), max(elev.TEMP[,1]), l = 50),
+      y = seq(min(elev.TEMP[,2]), max(elev.TEMP[,2]), l = 50), 
+      z = matrix(elev.TEMP$Elevation,nrow=50, byrow = T),
+      col="#FFE08080", labcex = 0.8, add = TRUE)
+image(x = seq(min(elev.TEMP[,1]), max(elev.TEMP[,1]), l = 50),
+      y = seq(min(elev.TEMP[,2]), max(elev.TEMP[,2]), l = 50),
+      z = matrix(elev.TEMP$Elevation,nrow=50, byrow = T),
+      col=viridis(64, alpha = 0.33), add = TRUE)
 #
 # end code
