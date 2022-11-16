8c654b6834e1bfca2a3c80b82e9b19c74fe96290
Andrew Rate <andrew.rate@uwa.edu.au>
2022-11-16 22:19:28 +0800

    edits to better visualise shapefile data

--- play-with-Ridgefield-shapefiles.R
+++ play-with-Ridgefield-shapefiles.R
@@ -32,10 +32,15 @@ unique(rf_landforms$MU_Soil)
 list.files(paste0(getwd(),"/Vegetation"))
 rf_veg <- read_sf(paste0(getwd(),"/Vegetation/Vegetation_gda94_utm50_EPSG28350.shp"))
 str(rf_veg)
+
+list.files(paste0(getwd(),"/Paddocks"))
+rf_paddocks <- read_sf(paste0(getwd(),"/Paddocks/Paddocks_gda94_utm50_EPSG28350.shp"))
+str(rf_paddocks)
+
 par(oma=c(4,4,2,22),mar=c(0,0,0,0))
-plot(st_geometry(rf_veg))
+plot(st_geometry(rf_veg),col=c(2,3,4,5,6,7,8))
 plot(st_geometry(rf_hydrology),col="blue",add=T)
-plot(st_geometry(rf_contours),col="gold",add=T)
+plot(st_geometry(rf_contours),col="chocolate",add=T)
 plot(rf_veg[9], max.plot=24, key.pos=4);axis(1);axis(2)
 head(st_coordinates(rf_veg))
 unique(rf_veg$MU_VEG)
@@ -50,6 +55,15 @@ plot(rf_roads[1], max.plot=24, xlim=c(499210,500280),
 plot(st_coordinates(rf_veg)[,1:2],asp=1,type="l")
 unique(rf_veg$MU_VEG)
 
+list.files(paste0(getwd(),"/Elevation")) # not point elevations for Ridgefield!
+rf_elevation_point <- read_sf(paste0(getwd(),"/Elevation/elevation_point.shp"))
+str(rf_elevation_point)
+par(oma=c(4,4,2,22),mar=c(0,0,0,0))
+plot(st_geometry(rf_elevation_point))
+plot(rf_elevation_point[22], max.plot=24)
+plot(st_coordinates(elevation_point)[,1:2],asp=1,type="l")
+unique(rf_veg$MU_VEG)
+
 require(ggmap)
 rf_gg <- get_googlemap(c(mean(lons0),mean(lats0)), maptype="satellite",zoom=13)
 ggmap(rf_gg)+
@@ -58,10 +72,25 @@ ggmap(rf_gg)+
   # geom_sf(data = rf_contours) +
   coord_sf(crs=st_crs(4326))
 
+str(rf_veg)
+unique(rf_veg$MU_LFORM)
+plot(rf_veg, max.plot=18)
+palette(colorRampPalette(c("#600000","#F0F0F0"))(6))
 ggplot() +
+  geom_sf(data = rf_veg, aes(fill=MU_LFORM)) +
   geom_sf(data = rf_hydrology, aes(col=SUB_TYPE)) +
-  scale_color_viridis_d() +
-  geom_path(mapping = aes(x=Longitude,y=Latitude),
-            data = rf_boundary,col="white") +
-  +
+  scale_fill_manual(values=palette()[c(2,4,1,3,5,6)]) +
+  scale_color_manual(values = c("#0000B0", "darkorchid")) +
+  # geom_path(mapping = aes(x=Longitude,y=Latitude),
+  #           data = rf_boundary[,3:4],col="#B0B0B080",size=2) +
+  xlim(116.945,117.007) +
+  ylim(-32.51,-32.468) +
+  coord_sf(crs=st_crs(4326)) +
   theme_bw()
+
+palette(rainbow(length(rf_veg$geometry),v=0.8))
+plot(st_coordinates(rf_veg$geometry[[1]]), xlim=c(495060,500570),
+     ylim=c(6403060,6407750),col="#00000000")
+for(i in 1:length(rf_veg$geometry)){
+  polygon(st_coordinates(rf_veg$geometry[[i]])[,1:2],border=i,lwd=1)
+}
