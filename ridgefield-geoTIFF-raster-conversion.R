3cb76b84652f008968e20914e353fb2f8cc5e5c8
Andrew Rate <andrew.rate@uwa.edu.au>
2022-11-18 14:03:18 +0800

    better colour palette for final plot

--- ridgefield-geoTIFF-raster-conversion.R
+++ ridgefield-geoTIFF-raster-conversion.R
@@ -1,5 +1,6 @@
 library(stars)
 library(sf)
+library(viridis)
 
 rf_DEM_stars <- read_stars("Ridgefield_DEM_gda94_utm50_epsg28350.tif")
 par(mar=c(0,0,0,0), oma=c(4,4,1,1))
@@ -22,12 +23,16 @@ par(mar=c(4,4,1,1), oma=c(0,0,0,0), mgp=c(1.7,0.3,0), tcl=0.25, font.lab=2)
 image(seq(495060,500572,len=425),
       seq(6403066,6407732,length.out=360),
       rf_elevs_mat[,seq(NCOL(rf_elevs_mat),1,-1)],
-      asp=1, col = terrain.colors(24)[1:18],
+      asp=1, col = cividis(18),
       xlab="Easting (UTM Zone 50, m)", ylab="Northing (UTM Zone 50, m)")
 contour(seq(495060,500572,len=425),
-      seq(6403066,6407732,length.out=360),
-      rf_elevs_mat[,seq(NCOL(rf_elevs_mat),1,-1)],
-      asp=1, levels = seq(290,400,5), add=T)
+        seq(6403066,6407732,length.out=360),
+        rf_elevs_mat[,seq(NCOL(rf_elevs_mat),1,-1)],
+        asp=1, levels = seq(335,400,5), add=T)
+contour(seq(495060,500572,len=425),
+        seq(6403066,6407732,length.out=360),
+        rf_elevs_mat[,seq(NCOL(rf_elevs_mat),1,-1)],
+        col="grey67", asp=1, levels = seq(290,330,5), add=T)
 lines(rf_boundary[,1:2])
 
 require(fields)
