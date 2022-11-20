01889de2f940ee878e9e10fd44f785af37497d26
Andrew Rate <andrew.rate@uwa.edu.au>
2022-11-20 08:01:51 +0800

    update to remove unneeded code

--- ridgefield-geoTIFF-raster-conversion.R
+++ ridgefield-geoTIFF-raster-conversion.R
@@ -1,29 +1,31 @@
 library(stars)
 library(sf)
 library(viridis)
+library(ggplot2)
 
 rf_DEM_stars <- read_stars("Ridgefield_DEM_gda94_utm50_epsg28350.tif")
 par(mar=c(0,0,0,0), oma=c(4,4,1,1))
 plot(rf_DEM_stars);axis(1);axis(2)
+names(rf_DEM_stars) <- "RF_elevation"
 str(rf_DEM_stars)
-names(rf_DEM_stars)
-names(rf_DEM_stars) <- "Ridgefield_DEM"
-str(rf_DEM_stars)
-st_dimensions(rf_DEM_stars)
+summary(st_coordinates(rf_DEM_stars))
+
+# might be useful to store the metadata for geoTIFF/stars object
+(rf_DEM_dims <- as.data.frame(rbind(unlist(rf_DEM_dims[1,]),
+                                    unlist(rf_DEM_dims[2,]))))
+rf_DEM_dims$from <- as.numeric(rf_DEM_dims$from)
+rf_DEM_dims$to <- as.numeric(rf_DEM_dims$to)
+rf_DEM_dims$offset <- as.numeric(rf_DEM_dims$offset)
+rf_DEM_dims$delta <- as.numeric(rf_DEM_dims$delta)
 ##   from  to  offset delta              refsys point values x/y
 ## x    1 425  495053    13 GDA94 / MGA zone 50 FALSE   NULL [x]
 ## y    1 360 6407739   -13 GDA94 / MGA zone 50 FALSE   NULL [y]
-rf_DEM_sf <- st_as_sf(rf_DEM_stars)
-# plot(rf_DEM_sf) # VERY SLOW!!!
-rf_DEMcontour <-
-  st_contour(rf_DEM_sf,  # doesn't work yet
-             contour_lines = TRUE, breaks=seq(280,380,10))
 
 par(mar=c(4,4,1,1), oma=c(0,0,0,0), mgp=c(1.7,0.3,0), tcl=0.25, font.lab=2)
 image(seq(495060,500572,len=425),
       seq(6403066,6407732,length.out=360),
       rf_elevs_mat[,seq(NCOL(rf_elevs_mat),1,-1)],
-      asp=1, col = cividis(18),
+      asp=1, col = cividis(18), # cividis seems best colour palette
       xlab="Easting (UTM Zone 50, m)", ylab="Northing (UTM Zone 50, m)")
 contour(seq(495060,500572,len=425),
         seq(6403066,6407732,length.out=360),
@@ -40,3 +42,11 @@ quilt.plot(x=st_coordinates(rf_DEM_stars)[,1],
            y=st_coordinates(rf_DEM_stars)[,2],
            z=rf_elevs_vect, asp=1,
            col=terrain.colors(36))
+
+# trying ggplot ####
+ggplot() +
+  geom_stars(data=rf_DEM_stars, na.rm=TRUE) +
+  geom_sf(data = rf_hydrology, col="steelblue2") +
+  coord_sf() +
+  scale_fill_viridis(option="cividis", na.value = "#00000000") +
+  theme_bw()
