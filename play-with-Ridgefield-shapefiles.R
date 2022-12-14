216c9a3b1252c31e33aac1778a290385fda89875
Andrew Rate <andrew.rate@uwa.edu.au>
2022-12-14 23:56:44 +0800

    reflow lines, add code for some GSWA, and DPIRD 2m contour, shapefiles

--- play-with-Ridgefield-shapefiles.R
+++ play-with-Ridgefield-shapefiles.R
@@ -12,17 +12,20 @@ write.csv(rf_boundary, file="rf_boundary_utm_LL.csv", row.names=FALSE)
 with(rf_boundary, plot(Longitude, Latitude, asp=1, type="l"))
 image(elev.TEMP[,1],elev.TEMP[,2],elev.TEMP[,3])
 
-rf_contours <- read_sf(paste0(getwd(),"/Contours/ContourLines_gda94_utm50_EPSG28350.shp"))
-plot(rf_contours[21], max.plot=24);axis(1);axis(2)
+rf_contours <-
+  read_sf(paste0(getwd(),"/Contours/ContourLines_gda94_utm50_EPSG28350.shp"))
+plot(st_convex_hull(rf_contours[2]), max.plot=24);axis(1);axis(2)
 head(st_coordinates(rf_contours))
 
-rf_hydrology <- read_sf(paste0(getwd(),"/Hydrology/RiverSystems_gda94_utm50_EPSG28350.shp"))
+rf_hydrology <-
+  read_sf(paste0(getwd(),"/Hydrology/RiverSystems_gda94_utm50_EPSG28350.shp"))
 str(rf_hydrology)
 plot(rf_hydrology[3], max.plot=24);axis(1);axis(2)
 head(st_coordinates(rf_hydrology))
 unique(rf_hydrology$DOMAIN_V1)
 
-rf_landforms <- read_sf(paste0(getwd(),"/Landforms/Landforms_gda94_utm50_EPSG28350.shp"))
+rf_landforms <-
+  read_sf(paste0(getwd(),"/Landforms/Landforms_gda94_utm50_EPSG28350.shp"))
 str(rf_landforms)
 par(oma=c(4,4,2,22),mar=c(0,0,0,0))
 plot(rf_landforms[7], max.plot=24, key.pos=4);axis(1);axis(2)
@@ -30,11 +33,13 @@ head(st_coordinates(rf_landforms))
 unique(rf_landforms$MU_Soil)
 
 list.files(paste0(getwd(),"/Vegetation"))
-rf_veg <- read_sf(paste0(getwd(),"/Vegetation/Vegetation_gda94_utm50_EPSG28350.shp"))
+rf_veg <-
+  read_sf(paste0(getwd(),"/Vegetation/Vegetation_gda94_utm50_EPSG28350.shp"))
 str(rf_veg)
 
 list.files(paste0(getwd(),"/Paddocks"))
-rf_paddocks <- read_sf(paste0(getwd(),"/Paddocks/Paddocks_gda94_utm50_EPSG28350.shp"))
+rf_paddocks <-
+  read_sf(paste0(getwd(),"/Paddocks/Paddocks_gda94_utm50_EPSG28350.shp"))
 str(rf_paddocks)
 
 par(oma=c(4,4,2,22),mar=c(0,0,0,0))
@@ -63,6 +68,34 @@ plot(st_geometry(rf_elevation_point))
 plot(rf_elevation_point[22], max.plot=24)
 plot(st_coordinates(elevation_point)[,1:2],asp=1,type="l")
 unique(rf_veg$MU_VEG)
+
+# GSWA shapefiles ####
+
+list.files(paste0(getwd(),"/50k_shp_INDEX"))
+index_50k <-
+  read_sf(paste0(getwd(),"/50k_shp_INDEX/index50.shp"))
+str(index_50k)
+index_50k_GDA94 <- st_transform(index_50k, crs=st_crs(28350))
+index_crop <- st_crop(index_50k, xmin=115.5, ymin=-33, xmax=117.6, ymax=-31.8)
+index_crop_GDA94 <- st_transform(index_crop, crs=st_crs(28350))
+plot(st_geometry(index_crop_GDA94))
+plot(rf_paddocks[2], add=TRUE, col="red2")
+plot(subset(index_50k_GDA94, index_50k_GDA94$SHEET_NAME=="PERTH"), add=T)
+plot(subset(index_50k_GDA94, index_50k_GDA94$SHEET_NAME=="PINJARRA"), add=T)
+plot(subset(index_50k_GDA94, index_50k_GDA94$SHEET_NAME=="MANDURAH"), add=T)
+plot(subset(index_50k_GDA94, index_50k_GDA94$SHEET_NAME=="ROCKINGHAM"), add=T)
+plot(subset(index_50k_GDA94, index_50k_GDA94$SHEET_NAME=="FREMANTLE"), add=T)
+plot(subset(index_50k_GDA94, index_50k_GDA94$SHEET_NAME=="MUNDARING"), add=T)
+plot(subset(index_50k_GDA94, index_50k_GDA94$SHEET_NAME=="PENCELL"), add=T)
+plot(rf_hydrology, add=TRUE, col="red2")
+
+list.files(paste0(getwd(),"/Pinjarra_geology"))
+pinj_geol <-
+  read_sf(paste0(getwd(),"/Pinjarra_geology/geology_surface_20321.shp"))
+str(pinj_geol)
+pinj_geol_GDA94 <- st_transform(pinj_geol, crs=st_crs(28350))
+st_bbox(pinj_geol_GDA94)
+st_bbox(rf_hydrology)
 
 require(ggmap)
 rf_gg <- get_googlemap(c(mean(lons0),mean(lats0)), maptype="satellite",zoom=13)
@@ -94,3 +127,17 @@ plot(st_coordinates(rf_veg$geometry[[1]]), xlim=c(495060,500570),
 for(i in 1:length(rf_veg$geometry)){
   polygon(st_coordinates(rf_veg$geometry[[i]])[,1:2],border=i,lwd=1)
 }
+
+# 2 metre contours from SLIP WA ####
+# https://catalogue.data.wa.gov.au/dataset/dpird-2-metre-contours
+
+list.files(paste0(getwd(),"/WA_2m_contours"))
+contours2m <-
+  read_sf(paste0(getwd(),"/WA_2m_contours/Contours_2_m_DPIRD_072.shp"))
+str(contours2m)
+paddocksGDA94 <- st_transform(rf_paddocks, crs=st_crs(4283))
+contours_RF <- st_crop(contours2m,st_bbox(paddocksGDA94))
+contours_RF2m <- st_intersection(contours_RF,paddocksGDA94)
+rm(contours2m)
+save.image()
+plot(st_geometry(contours_RF2m))
